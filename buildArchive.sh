7cfc038cdbd991d7ee8fec5f71c5cfb3f24d84a8
yohaku <yohaku@4-89.life>
2020-11-27 01:49:07 +0900

    replace article files and fix build script

--- buildArchive.sh
+++ buildArchive.sh
@@ -1,2 +1,1 @@
-mkdir ./dist/articles
-processmd "./src/assets/articles/md/*" --stdout --preview 200 --outputDir "./dist/articles" > "./src/assets/articles/archive.json"
+processmd "./public/articles/md/*" --stdout --preview 200 --outputDir "./public/articles/json" > "./public/articles/archive.json"
