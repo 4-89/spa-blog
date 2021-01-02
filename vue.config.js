7eeef9ac142b27e2e43dd159405e65b63598aa8d
yohaku <yohaku@4-89.life>
2021-01-02 21:53:13 +0900

    css to less

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,15 @@
+const path = require("path");
 module.exports = {
   publicPath: "/",
   pages: {
     index: {
       entry: "src/main.js",
       title: "n"
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      modules: [path.resolve("./src/assets/less")]
     }
   }
 };
